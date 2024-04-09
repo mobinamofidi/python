407a77590286fcb9c735bf554170993dbf7cf220
mobinamofidi <166532736+mobinamofidi@users.noreply.github.com>
2024-04-09 17:39:40 +0200

    Add files via upload

---
+++ control loop.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 10:38:13 2024
+
+@author: mobin
+"""
+
+#control loop
+
+#while True :
+     # age= int(input("YOUR AGE:"))
+       #if age >= 20 :
+          # print("you are not accepted ")
+           
+          
+phone_number="0938-783-53"
+        
+for i in phone_number :
+      if i == "-":
+       continue
+      print(i,end=" ")           
+           
+   
+    
